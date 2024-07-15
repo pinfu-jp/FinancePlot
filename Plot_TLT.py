67994a44e049e9e24d7759ddc215706b6c695341
h.a <hidetaka.asahi.3@gmail.com>
2024-07-15 20:41:55 +0900

    ドル円追加

--- Plot_TLT.py
+++ Plot_TLT.py
@@ -1,3 +1,3 @@
 from yfinance_func import plot_from_yfinance
 
-plot_from_yfinance('米国債20年超ETF', 'TLT', 20)
+plot_from_yfinance('米国債20年超ETF', 'TLT', 30)
---
+++ Plot_Yen.py
@@ -0,0 +1,3 @@
+from yfinance_func import plot_from_yfinance
+
+plot_from_yfinance('円/ドル', 'JPY=X', 30)
